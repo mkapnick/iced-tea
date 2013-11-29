44b98c3d0741d1bc3e4680ce3a6d8cb796ecbf75
hardgrdb <hardgrdb@dukes.jmu.edu>
2013-11-29 17:48:47 -0500

    Messed with MenuController to help with flow of dialogue

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,27 +1,63 @@
 package controller;
 
 import model.EventNode;
+import scene.visual.content.ChoiceContent;
 import scene.visual.content.MenuContent;
 
 public class MenuController
 {
 	private EventNode<MenuContent> currentNode;
-	private int numToRender;
 	
 	public MenuController(EventNode<MenuContent> content, int numToRender)
 	{
 		this.currentNode = content;
-		this.numToRender = numToRender;
 	}
 	
-	public int getNumToRender()
+	public void advanceContent()
 	{
-		return this.numToRender;
+		EventNode<MenuContent> parent;
+		int currentIndex;
+		parent = currentNode.getParent();
+		
+		currentIndex = parent.getIndex(currentNode);
+		
+		if (currentIndex < parent.getChildCount() - 1)
+			currentIndex++;
+		
+		currentNode = parent.getChildAt(currentIndex);
 	}
 	
 	public MenuContent getCurrentContent()
 	{
 		return currentNode.getElement();
+	}
+	
+	public void setCurrentContent(EventNode<MenuContent> content)
+	{
+		this.currentNode = content;
+	}
+	
+	public String getCurrentContentType()
+	{
+		if (currentNode.getElement() instanceof ChoiceContent)
+			return "ChoiceContent";
+		
+		return "DialogueContent";
+	}
+	
+	public void reset()
+	{
+		if (currentNode.getParent() != null)
+		{
+			currentNode = currentNode.getParent();
+			reset();
+		}
+	}
+	
+	public void setCurrentContentToFirstChild()
+	{
+		if (currentNode.getChildCount() > 0)
+			currentNode = currentNode.getChildAt(0);
 	}
 	
 	
